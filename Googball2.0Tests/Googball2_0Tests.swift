d3414aa72814e08a41f0a48d3faedb58350609f3
LoganGough <logan.gough26@d255.org>
2025-02-26 07:40:53 -0600

    Initial Commit

---
+++ Googball2.0Tests/Googball2_0Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Googball2_0Tests.swift
+//  Googball2.0Tests
+//
+//  Created by LOGAN GOUGH on 2/26/25.
+//
+
+import Testing
+@testable import Googball2_0
+
+struct Googball2_0Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
